a48b96114775bc3814efedf4a2930a8f2c0171b0
Joseph Watts <josephcw@live.com>
2018-08-30 12:02:11 -0500

    Update plugin for craftbukkit 1.13.1

--- src/com/github/_josephcw/hiddenlorax/LoraxTimer.java
+++ src/com/github/_josephcw/hiddenlorax/LoraxTimer.java
@@ -1,12 +1,13 @@
 package com.github._josephcw.hiddenlorax;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.Map.Entry;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
@@ -18,7 +19,8 @@ public class LoraxTimer extends BukkitRunnable {
 	
 	HiddenLoraxMain plugin;
 	List<Entity> serverEntities = new ArrayList<>();
-	HashMap<Entity, Byte> entityBytes = new HashMap<>();
+	HashSet<Entity> saplingEntities = new HashSet<>();
+	HashSet<Material> saplingTypes = new HashSet<>();
 	
 	private Item sItem;
 	private ItemStack sItemStack;
@@ -29,16 +31,17 @@ public class LoraxTimer extends BukkitRunnable {
 
 	@Override
 	public void run() {
+		saplingTypes.addAll(Arrays.asList(Material.ACACIA_SAPLING, Material.SPRUCE_SAPLING, Material.BIRCH_SAPLING, Material.DARK_OAK_SAPLING, Material.JUNGLE_SAPLING, Material.OAK_SAPLING));
 		// Not necessary, just catches weird errors,
 		// Possible wrong world's entities pulled
-		if (serverEntities.size() > 0) {
+		if (!serverEntities.isEmpty()) {
 			for (Entity sEntity : serverEntities) {
 				if (assignAsItem(sEntity)) {
 					if (itemStackIsSapling(sItemStack)) {
 						Block blockBeneath = getBlockBeneath(sEntity);
 						if (allowedSurface(blockBeneath)) {
 							if (currentBlockAllowed(sEntity)) {
-								entityBytes.put(sEntity, getTreeByteFromName(sItem));
+								saplingEntities.add(sEntity);
 							}
 						}
 					}
@@ -47,36 +50,32 @@ public class LoraxTimer extends BukkitRunnable {
 			// only if there are entities, then run the plant trees method.
 			// lambda to replace runnable anonymous inner class
 			Runnable r = () -> { 
-				plantTrees(entityBytes); 
+				plantTrees(saplingEntities); 
 				
 				};
 			plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, r, 20L);
 		}
 	}
 	
-
-	@SuppressWarnings("deprecation")
-	public void plantTrees(HashMap<Entity, Byte> myMap) {
-		if (!myMap.isEmpty()) {
-			Set<Entry<Entity, Byte>> set = myMap.entrySet();
-			Iterator<Entry<Entity, Byte>> iterator = set.iterator();
+	public void plantTrees(Set<Entity> saplings) {
+		if (!saplings.isEmpty()) {
+			Iterator<Entity> iterator = saplings.iterator();
 			
 			while(iterator.hasNext()) {
-				Entry<Entity, Byte> me = iterator.next();
-				Block blockToSapling = me.getKey().getLocation().getBlock();
+				Entity me = iterator.next();
+				Block blockToSapling = me.getLocation().getBlock();
 				
 				/*
 				 * We need to check to see if the block is still valid now
 				 * that we are on a syncronus task with the server.
 				 */
-				if (currentBlockAllowed(me.getKey())) {
+				if (currentBlockAllowed(me)) {
 					// We also need to check that the block beneath is still allowed.
-					if (allowedSurface(getBlockBeneath(me.getKey()))) { 
-						blockToSapling.setType(Material.SAPLING);
-						blockToSapling.setData(me.getValue());
+					if (allowedSurface(getBlockBeneath(me))) { 
+						blockToSapling.setType(getSaplingMaterialFromItem((Item) me));
 					}
 				}
-				me.getKey().remove();
+				me.remove();
 			}
 		}
 	}	
@@ -85,30 +84,13 @@ public class LoraxTimer extends BukkitRunnable {
 		return sEntity.getLocation().getBlock().getType().equals(Material.AIR);
 	}
 
-	private Byte getTreeByteFromName(Item treeSapling) {
-		// item.tile.sapling.XYZ
-		switch(treeSapling.getName().substring(18)) {
-		case "spruce":
-			return (byte) 1;
-		case "birch":
-			return (byte) 2;
-		case "jungle":
-			return (byte) 3;
-		case "acacia":
-			return (byte) 4;
-		case "big_oak":
-			return (byte) 5;
-		default:
-			return (byte) 0;
-		}
+	private Material getSaplingMaterialFromItem(Item treeSapling) {
+		return treeSapling.getItemStack().getType();
 	}
-
+	
 	private boolean allowedSurface(Block blockBeneathSapling) {
-		if (blockBeneathSapling.getType().equals(Material.GRASS) ||
-				blockBeneathSapling.getType().equals(Material.DIRT)) { 
-			return true;
-		}
-		return false;
+		return (blockBeneathSapling.getType().equals(Material.GRASS_BLOCK) ||
+				blockBeneathSapling.getType().equals(Material.DIRT));
 	}
 
 	private Block getBlockBeneath(Entity sEntity) {
@@ -116,10 +98,7 @@ public class LoraxTimer extends BukkitRunnable {
 	}
 
 	private boolean itemStackIsSapling(ItemStack sItemStack2) {
-		if (sItemStack2.getData().getItemType().equals(Material.SAPLING)) {
-			return true;
-		}
-		return false;
+		return saplingTypes.contains(sItemStack2.getType());
 	}
 
 	private boolean assignAsItem(Entity sEntity) {
